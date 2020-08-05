fec3cbfacc2fb310d95641f3eb26119ff2300356
steveski <steve@steve-york.com>
2020-08-05 21:34:29 +1000

    Implemented new Query methods taking expressions for Include statements and update example code to illustrate usage. Also created an AddressView with Projector expression for when doing Sql Server side project. Also added a projector to the GetCustomerWithAddressView

--- Perigee.Framework.Base/Database/IReadUnfilteredEntities.cs
+++ Perigee.Framework.Base/Database/IReadUnfilteredEntities.cs
@@ -1,6 +1,9 @@
 ﻿namespace Perigee.Framework.Base.Database
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using Perigee.Framework.Base.Entities;
 
     public interface IReadUnfilteredEntities
@@ -8,5 +11,8 @@
         IQueryable<TEntity> QueryUnfiltered<TEntity>() where TEntity : class, IEntity;
         IQueryable<TEntity> QueryUnfiltered<TEntity>(bool includeSoftDeleted) where TEntity : class, IEntity, ISoftDelete;
         
+        IQueryable<TEntity> QueryUnfiltered<TEntity, TProperty>(IEnumerable<Expression<Func<TEntity, TProperty>>> includes) where TEntity : class, IEntity;
+        IQueryable<TEntity> QueryUnfiltered<TEntity, TProperty>(IEnumerable<Expression<Func<TEntity, TProperty>>> includes, bool includeSoftDeleted) where TEntity : class, IEntity, ISoftDelete;
+
     }
 }

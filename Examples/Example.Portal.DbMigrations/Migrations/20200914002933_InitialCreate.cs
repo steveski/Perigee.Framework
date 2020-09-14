b3095f9ba67bc4b44d381db24cf1012a5edd5291
Darryl Hunter <darryl.hunter@codersoft.com.au>
2020-09-14 10:31:12 +1000

    Updated InitialCreate migration.

---
+++ Examples/Example.Portal.DbMigrations/Migrations/20200914002933_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Example.Portal.DbMigrations.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    customerName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Version = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ Examples/Example.Portal/Pages/Customers/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Example.Portal.Entities;
+using Example.Portal.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Example.Portal.Pages.Customers
+{
+    [Authorize(Roles = "Admin")]
+    public class IndexModel : PageModel
+    {
+        private readonly ICustomerDataService _customerDataService;
+        public List<Customer> customers { get; set; }
+
+        public IndexModel(ICustomerDataService customerDataService)
+        {
+            _customerDataService = customerDataService;
+        }
+
+        public async System.Threading.Tasks.Task OnGetAsync()
+        {
+            customers = (await _customerDataService.getCustomers()).ToList();
+        }
+    }
+}
